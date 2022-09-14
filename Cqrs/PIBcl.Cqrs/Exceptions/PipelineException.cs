3f7041c05332a8eb715eaaacb15ba55d390f992e
Tiago Lopes Costa <tiago.costa@ax4b.com>
2022-09-14 09:41:19 -0300

    Refactor: renamed and deleted Cqrs/duCorp to Cqrs/PIBcl.

---
+++ Cqrs/PIBcl.Cqrs/Exceptions/PipelineException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PIBcl.Cqrs.Exceptions
+{
+   public class PipelineException : Exception
+   {
+      public PipelineException(string message, Exception innerException)
+          : base(message, innerException)
+      {
+      }
+   }
+}
