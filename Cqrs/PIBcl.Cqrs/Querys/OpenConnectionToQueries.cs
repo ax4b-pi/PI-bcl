000ed5a8966617798eb4b1c888a0fba76b1d2378
tiagoLopesAX <tiago.costa@ax4b.com>
2022-10-04 11:33:32 -0300

    Feat - method queries entity added.

---
+++ Cqrs/PIBcl.Cqrs/Querys/OpenConnectionToQueries.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using MySqlConnector;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PIBcl.Cqrs.Querys
+{
+    public class OpenConnectionToQueries<T> where T : class
+    {
+        public static async Task<IEnumerable<T>> QueryEntity(string _connection, string query, string conditionQuery = "", object parameters = null) 
+        {
+            MySqlConnection mySqlConnection = new MySqlConnection(_connection);
+            using (var connection = mySqlConnection)
+            {
+                IEnumerable<T> entity;
+
+                try
+                {
+                    entity = await connection.QueryAsync<T>("" + query+" " + conditionQuery, parameters);
+                }
+                catch (System.Exception ex)
+                {
+                    return null;
+                }
+                return null;
+            }
+        }
+    }
+}
