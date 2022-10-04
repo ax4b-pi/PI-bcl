fedf7c00cbb503e6fedcf0c2f509d1685dba3b50
tiagoLopesAX <tiago.costa@ax4b.com>
2022-10-04 14:39:51 -0300

    Refactor - method changings.

---
+++ Cqrs/PIBcl.Cqrs/Querys/ConnectionDatabase.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PIBcl.Cqrs.Querys
+{
+    public class ConnectionDatabase<T> where T : class
+    {
+        private readonly MySqlConnection _connection;
+        public ConnectionDatabase(MySqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<IEnumerable<T>> QueryEntity(string query, object parameters = null) 
+        {
+            using (_connection)
+            {
+                IEnumerable<T> entity;
+
+                try
+                {
+                    entity = await _connection.QueryAsync<T>(query, parameters);
+                }
+                catch (System.Exception ex)
+                {
+                    return null;
+                }
+                return entity;
+            }
+        }
+        public async Task<bool> ManipulationEntity(string query, object parameters = null)
+        {
+            using (_connection)
+            {
+                try
+                {                    
+                    var entity = await _connection.ExecuteAsync(query, parameters);
+                }
+                catch (System.Exception ex)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
