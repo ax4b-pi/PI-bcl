e03ce46c3166bc01690b5af3f7945b0719f90382
tiagoLopesAX <tiago.costa@ax4b.com>
2022-10-05 12:06:22 -0300

    Refactor - update instance MySqlConnection.

--- Cqrs/PIBcl.Cqrs/Querys/DatabaseConnection.cs
+++ Cqrs/PIBcl.Cqrs/Querys/DatabaseConnection.cs
@@ -1,6 +1,6 @@
 ﻿using Dapper;
+using Microsoft.Extensions.Configuration;
 using MySqlConnector;
-using PIBcl.Cqrs.SeedWork;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -8,12 +8,18 @@ namespace PIBcl.Cqrs.Querys
 {
     public class DatabaseConnection<T> where T : class
     {
+
         private readonly MySqlConnection _connection;
-        public DatabaseConnection(string connection)
+        private IConfiguration _configuration;
+        private string _connectionString { get { return _configuration.GetConnectionString("DefaultConnection"); } }
+
+        public DatabaseConnection(IConfiguration configuration)
         {
-            _connection = new MySqlConnection(connection);
+            _configuration = configuration;
+            _connection = new MySqlConnection(_connectionString);
         }
-        public async Task<IEnumerable<T>> QueryEntity(string query, object parameters = null) 
+
+        public async Task<IEnumerable<T>> QueryEntity(string query, object parameters = null)
         {
             using (_connection)
             {
@@ -32,13 +38,13 @@ namespace PIBcl.Cqrs.Querys
         }
         public async Task<bool> ManipulationEntity(string query, object parameters = null)
         {
-            using (_connection)
+            using (var connectionMethod = new MySqlConnection(_connectionString))
             {
                 int entity;
 
                 try
-                {                    
-                    entity = await _connection.ExecuteAsync(query, parameters);
+                {
+                    entity = await connectionMethod.ExecuteAsync(query, parameters);
                 }
                 catch (System.Exception ex)
                 {
