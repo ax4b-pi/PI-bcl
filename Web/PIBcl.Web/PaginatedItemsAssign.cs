a7eb4249cc0593beef4d67ac92760aa607c647bd
tiagoLopesAX <tiago.costa@ax4b.com>
2022-11-25 11:11:44 -0300

    Feat - paginated itens to gateway

---
+++ Web/PIBcl.Web/PaginatedItemsAssign.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace PIBcl.Web
+{
+    public class PaginatedItemsAssign<TEntity> where TEntity : class
+    {
+        public int PageIndex { get; private set; }
+
+        public int PageSize { get; private set; }
+
+        public long Count { get; private set; }
+
+        public bool MoreRecords { get; set; }
+
+        public IEnumerable<TEntity> Data { get; set; }
+
+        public PaginatedItemsAssign(int pageIndex, int pageSize, long count,
+            IEnumerable<TEntity> data, bool moreRecords)
+        {
+            this.PageIndex = pageIndex;
+            this.PageSize = pageSize;
+            this.Count = count;
+            this.Data = data;
+            this.MoreRecords = moreRecords;
+        }
+    }
+}
